4e650307fd9b265259f24cbcafc4ae43119dd56c
Lawrence Pan <lawrencefeipan@gmail.com>
2016-04-09 21:29:28 -0400

    add icon

--- server/server.js
+++ server/server.js
@@ -40,6 +40,7 @@ const renderFullPage = (html) => {
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="/img/icon.png" />
         <title>Find a Classroom</title>
         <link rel="stylesheet" href=${cssPath} />
       </head>
